9d19077b5afb2b0ff0101945cac6de4c505a188d
Lucas de Oliveira Lima <contatelucasdeoliveira@gmail.com>
2024-11-23 19:07:26 -0300

    organizaçao das noticias  por estado

--- src/data/newsData.ts
+++ src/data/newsData.ts
@@ -6,7 +6,7 @@ export const noticias: Noticia[] = [
     userId: "5f678b12-23ef-8012-4567-89abcdef1234",
     title: "De olho no meio-ambiente",
     content: "Brasil importa fertilizante para impulsionar descarbonização do agronegócio",
-    state: "MG",
+    state: "AC",
     city: "Enstone",
     image:
       "https://images.unsplash.com/photo-1645459264002-c80d2b5d2d23?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -17,7 +17,7 @@ export const noticias: Noticia[] = [
     userId: "2c345e78-90bc-5def-1234-56789abcdef0",
     title: "Exportações em alta",
     content: "Embarques de café sobem mais de 40% e superam US$ 1 bilhão",
-    state: "SP",
+    state: "AL",
     city: "New York",
     image:
       "https://images.unsplash.com/photo-1694500788249-71b09a87db40?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -27,7 +27,7 @@ export const noticias: Noticia[] = [
     userId: "3d456f89-01cd-6ef0-2345-6789abcdef12",
     title: "Desafios do setor",
     content: "Agricultura busca transformação para enfrentar mudanças climáticas",
-    state: "SP",
+    state: "AP",
     city: "Austin",
     image:
       "https://plus.unsplash.com/premium_photo-1661962692059-55d5a4319814?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -37,7 +37,7 @@ export const noticias: Noticia[] = [
     userId: "4e567a01-12de-7f01-3456-789abcdef123",
     title: "Bahia consolida posição como a próxima fronteira do agronegócio no Brasil",
     content: "Colaborações visam a qualificação e profissionalização de produtores locais, uma área que ainda demanda atenção no estado.",
-    state: "RJ",
+    state: "AM",
     city: "Maranello",
     image:
       "https://images.unsplash.com/photo-1633375011368-b3d9b70ceef8?q=80&w=1430&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -48,7 +48,7 @@ export const noticias: Noticia[] = [
     userId: "6a789c23-34f0-9134-5678-9abcdef12345",
     title: "Pesquisa aponta contaminação de população por agrotóxicos em MT",
     content: "Estudo feito entre 2015 e 2019 aponta pelo menos 13 morbidades associadas à exposição da população a agrotóxicos",
-    state: "RS",
+    state: "BA",
     city: "Woking",
     image:
       "https://images.unsplash.com/photo-1698610642784-2e5432c1ecc9?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -59,7 +59,7 @@ export const noticias: Noticia[] = [
     userId: "1b234d67-89ab-4cde-0123-456789abcdef",
     title: "Cadeia do mel em Pernambuco ganha força com unidade de extração em Dormentes",
     content: "A Companhia do Desenvolvimento dos Vales do São Francisco e do Parnaíba (Codevasf) quer destravar gargalos que reduzem o potencial da apicultura em Pernambuco. ",
-    state: "PE",
+    state: "CE",
     city: "San Francisco",
     image:
       "https://images.unsplash.com/photo-1647427062468-74ff21e8934f?q=80&w=1590&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -70,7 +70,7 @@ export const noticias: Noticia[] = [
     userId: "1b234d67-89ab-4cde-0123-456789abcdef",
     title: "E-Agro Salvador 2024 conecta o agronegócio baiano à cidade",
     content: "O evento, que segue até sábado (9), reúne uma diversidade de produtos e soluções para o agronegócio",
-    state: "BA",
+    state: "DF",
     city: "San Francisco",
     image:
       "https://assets.revistacultivar.com.br/88987afa-0acd-4976-9d53-63f23aaf3b2f.jpg",
@@ -81,7 +81,7 @@ export const noticias: Noticia[] = [
     userId: "1b234d67-89ab-4cde-0123-456789abcdef",
     title: "Evento debate custos de produção de grãos e competitividade do agro",
     content: "Painéis da manhã do “Benchmark Agro” reuniram especialistas e pesquisadores",
-    state: "DF",
+    state: "ES",
     city: "San Francisco",
     image:
       "https://cnabrasil.org.br/storage/arquivos/WhatsApp-Image-2024-11-05-at-10.11.29.jpeg",
@@ -92,7 +92,7 @@ export const noticias: Noticia[] = [
     userId: "1b234d67-89ab-4cde-0123-456789abcdef",
     title: "1° Evento Agrocapitais 2024",
     content: "A Confederação da Agricultura e Pecuária do Brasil (CNA) e o Instituto Brasileiro de Direito do Agronegócio (IBDA) promovem, no dia 05 de setembro, no auditório da CNA, em Brasília, o workshop “O Agro e o Mercado de Capitais",
-    state: "DF",
+    state: "GO",
     city: "San Francisco",
     image:
       "https://cnabrasil.org.br/storage/arquivos/icones/39384869954_70727d1098_k.jpg",
@@ -114,7 +114,7 @@ export const noticias: Noticia[] = [
     userId: "1b234d67-89ab-4cde-0123-456789abcdef",
     title: "Agro é parte da solução para os desafios climáticos, diz presidente da CNA",
     content: "Confederação entregou posicionamento do setor no evento “Pré-COP 29 – De Baku a Belém”, na sede da entidade",
-    state: "DF",
+    state: "MT",
     city: "San Francisco",
     image:
       "https://www.cnabrasil.org.br/storage/arquivos/WhatsApp-Image-2024-10-09-at-10.29.31.jpg",
@@ -125,7 +125,7 @@ export const noticias: Noticia[] = [
     userId: "1b234d67-89ab-4cde-0123-456789abcdef",
     title: "Calendário de feiras agropecuárias 2024",
     content: "Expodireto Cotrijal, em Não-me-toque (RS), é um dos principais destaques do mês",
-    state: "RS",
+    state: "MS",
     city: "San Francisco",
     image:
       "https://s2-globorural.glbimg.com/Mlp3NUWTU6F36Muu9mL2_yy2lz4=/0x0:4903x2755/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_afe5c125c3bb42f0b5ae633b58923923/internal_photos/bs/2024/R/u/AqXhAISY24YZlTTDdJZw/extranet-gallery-17364.jpg",
@@ -136,7 +136,7 @@ export const noticias: Noticia[] = [
     userId: "1b234d67-89ab-4cde-0123-456789abcdef",
     title: "Fazenda com 4 quatros para aluguel",
     content: "10000m², 4 quartos, 6 banheiros, 50 vagas",
-    state: "RS",
+    state: "MG",
     city: "San Francisco",
     image: "https://fazendacapoava.com.br/wp-content/webp-express/webp-images/uploads/2023/05/SITE-HOME-3-1.jpg.webp",
   },
@@ -146,7 +146,7 @@ export const noticias: Noticia[] = [
     userId: "1b234d67-89ab-4cde-0123-456789abcdef",
     title: "Chácara com 2 quartos à venda",
     content: "100m², 2 quartos, 2 banheiros, 20 vagas",
-    state: "PE",
+    state: "PA",
     city: "San Francisco",
     image: "https://festivalvaledocafe.com.br/wp-content/themes/inspiro/assets/images/fazendas/fazenda-santarosa.jpg",
   },
@@ -166,8 +166,10 @@ export const noticias: Noticia[] = [
     userId: "1b234d67-89ab-4cde-0123-456789abcdef",
     title: "Fazenda com 6 quartos para aluguel",
     content: "600m², 6 quartos, 4 banheiros, 10 vagas",
-    state: "PE",
+    state: "PI",
     city: "San Francisco",
     image: "https://www.casadevalentina.com.br/wp-content/uploads/2022/11/Sede-de-um-fazenda-em-Ribeira%CC%83o-dos-I%CC%81ndios-com-1300m2-de-a%CC%81rea-construi%CC%81da-projetada-pelo-arquiteto-GUSTAVO-MARASCA-_-foto-18.jpg.optimal.jpg",
   },
+
+  
 ];
